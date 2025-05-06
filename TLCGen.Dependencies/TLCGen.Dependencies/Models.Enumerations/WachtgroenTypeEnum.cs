445c29b5368d003106cfd6d06dbabf27343a43a4
LexTrafico <d.denouden@ll-t.nl>
2025-05-07 00:57:36 +0200

    peloton koppeling (RW bit / IH)

---
+++ TLCGen.Dependencies/TLCGen.Dependencies/Models.Enumerations/WachtgroenTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using TLCGen.Helpers;
+
+namespace TLCGen.Models.Enumerations
+{
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
+    public enum WachtgroenTypeEnum
+    {
+        [Description("Geen")]
+        GroenVasthouden,
+        [Description("WSG zonder aanvraag")]
+        GroenVasthouden,
+        [Description("WSG met aanvraag")]
+        GroenVasthoudenEnAanvragen,
+    }
+}
